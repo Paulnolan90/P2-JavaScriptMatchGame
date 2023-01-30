1dbf0314e1e8ceadd016219d0c26978e775613d5
Paulnolan90 <paulnolan1990@gmail.com>
2023-01-30 16:42:25 +0000

    adding images to js board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
     //images for cards
-    const cardArray = [
+  const cardArray = [
         {
-            name:'Haaland'
+            name:'Haaland',
             img: 'images/Haaland.jpg'
-        }
+        },
         {
-            name:'Mbappe'
+            name:'Haaland',
+            img: 'images/Haaland.jpg'
+        },
+        {
+            name:'Mbappe',
             img: 'images/Mbappe.jpg'
-        }
+        },
         {
-            name:'Messi'
+            name:'Mbappe',
+            img: 'images/Mbappe.jpg'
+        },
+        {
+            name:'Messi',
             img: 'images/Messi.jpg'
-        }
+        },
         {
-            name:'Ronaldo'
+            name:'Messi',
+            img: 'images/Messi.jpg'
+        },
+        {
+            name:'Ronaldo',
             img: 'images/Ronaldo.jpg'
-        }
+        },
         {
-            name:'Neymar'
+            name:'Ronaldo',
+            img: 'images/Ronaldo.jpg'
+        },
+        {
+            name:'Neymar',
             img: 'images/Neymar.jpg'
-        }
+        },
         {
-            name:'Rashford'
+            name:'Neymar',
+            img: 'images/Neymar.jpg'
+        },
+        {
+            name:'Rashford',
+            img: 'images/Rashford.jpg'
+        },
+        {
+            name:'Rashford',
             img: 'images/Rashford.jpg'
         }
     ]
-
-    const grid = document.querySelector.apply('.grid')
+//creating board
+    const grid = document.querySelector('.grid')
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
-            card.setAttribute('src', 'imagees/blank.png')
+            card.setAttribute('src', 'assets/images/Blank.jpg')
             card.setAttribute('data-id', i)
             //event listener
-            
+            grid.appendChild(card)
         }
     }
-
-}
+createBoard()
+})
